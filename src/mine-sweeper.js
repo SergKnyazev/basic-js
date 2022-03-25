7b797b43a1b61317c5fad9424f52e6a26f15f04b
SergKnyazev <serg.knyazev77@gmail.com>
2022-03-25 14:40:54 +0300

    feat: finish mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,12 +23,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(matrix) {
+ function minesweeper(matrix) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const resultArr = [];
+  const columnLength = matrix.length;
+  const rowLength = matrix[0].length;
 
-
-  
+  for (let i = 0; i < columnLength; i++) {
+    const rowArr = [];
+    for (let j = 0; j < rowLength; j++) {
+      let countMines = 0;
+      if (i - 1 >= 0 && j - 1 >= 0 && matrix[i - 1][j - 1]) countMines += 1;
+      if (i - 1 >= 0 && matrix[i - 1][j]) countMines += 1;
+      if (i - 1 >= 0 && j + 1 < rowLength && matrix[i - 1][j + 1]) countMines += 1;
+      if (j - 1 >= 0 && matrix[i][j - 1]) countMines += 1;
+      if (j + 1 < rowLength && matrix[i][j + 1]) countMines += 1;
+      if (i + 1 < columnLength && j - 1 >= 0 && matrix[i + 1][j - 1]) countMines += 1;
+      if (i + 1 < columnLength && matrix[i + 1][j]) countMines += 1;
+      if (i + 1 < columnLength && j + 1 < rowLength && matrix[i + 1][j + 1]) countMines += 1;
+      rowArr.push(countMines)
+    }
+    resultArr.push(rowArr)
+  }
+  return resultArr 
 }
 
 module.exports = {
