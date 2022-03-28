0f6aae2303610ee90e235f2c355014be1c56e41b
SergKnyazev <serg.knyazev77@gmail.com>
2022-03-28 14:50:18 +0300

    feat: finish encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
 function encodeLine(str) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let countChars = 1;
+  let encodeStr = '';
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      countChars += 1;
+    } else {
+      encodeStr += countChars > 1 ? String(countChars) + str[i] : str[i];
+      countChars = 1;
+    }
+  }
 
-  
+  return encodeStr;
 }
 
 module.exports = {
